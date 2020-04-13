1bb0323c6b87fa813d484a7ea5d515a868d4acfc
Huyghe Kris <Kris.huyghe@student.howest.be>
2020-04-13 17:03:06 +0200

    Add deliverys  to "inbehandeling"

--- HoWestPost.Domain/Delivery.cs
+++ HoWestPost.Domain/Delivery.cs
@@ -8,11 +8,22 @@ namespace HoWestPost.Domain
     {
         private PackageType packageType;
         private int travelTime;
+        private bool prior;
+        private DateTime startTime;
+        private int deliveryNumber; 
 
-        public Delivery(PackageType packageType, int travelTimeToDestination)
+        public Delivery(PackageType packageType, int travelTimeToDestination , bool prior, int deliveryNumber)
         {
             this.packageType = packageType;
             this.travelTime = travelTimeToDestination;
+            this.prior = prior;
+            startTime = DateTime.Now;
+            this.deliveryNumber = deliveryNumber;
+        }
+        public override string ToString()
+        {
+            string ListText = $"nr={deliveryNumber} Prior={prior} Type ={packageType} Time={travelTime}";
+            return ListText;
         }
     }
 }
--- HoWestPost.UI/MainWindow.xaml.cs
+++ HoWestPost.UI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using DocumentFormat.OpenXml.Spreadsheet;
+using HoWestPost.Domain;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,9 +24,12 @@ namespace HoWestPost.UI
     public partial class MainWindow : Window
     {
         int counter = 30;
-        int deliveryTime = 0;
-       
-       
+        int deliveryTime = 30;
+        bool prior = false;
+        int deliveryNumber = 0;
+        ArrayList deliveries = new ArrayList();
+     //   List deliveries = new List();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -41,9 +46,44 @@ namespace HoWestPost.UI
 
         private void ComboxDeliveryTime_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int selectedIndex = ComboxDeliveryTime.SelectedIndex;
+         
             deliveryTime = Int32.Parse(ComboxDeliveryTime.SelectedItem.ToString());
             
         }
+
+        private void ButtonMini_Click(object sender, RoutedEventArgs e)
+        {
+            AddToWaitinglist(PackageType.Mini);
+        }
+        private void ButtonStandaard_Click(object sender, RoutedEventArgs e)
+        {
+            AddToWaitinglist(PackageType.Standard);
+        }
+        private void buttonMaxi_Click(object sender, RoutedEventArgs e)
+        {
+            AddToWaitinglist(PackageType.Maxi);
+        }
+        
+
+        private void CheckBoxPrior_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+        void AddToWaitinglist(PackageType packageType)
+        {
+            deliveryNumber++;
+            var delivery = new Delivery(packageType, deliveryTime, CheckBoxPrior.IsChecked.Value, deliveryNumber);
+            //  deliveries.Append(delivery);
+            deliveries.Add(delivery);
+         
+          //  deliveries = deliveries.OrderBy(x => x.Name).ToArray();
+           // deliveries = deliveries.orderby()
+            ListBoxWaiting.Items.Clear();
+            foreach (Delivery d in deliveries)
+            {
+                ListBoxWaiting.Items.Add(d);
+            }
+        }
+
     }
 }
