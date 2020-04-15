7ee8beb552ab35feb123bb91431397ed6674a260
Huyghe Kris <Kris.huyghe@student.howest.be>
2020-04-15 23:35:14 +0200

    Add more fuction to deliveryprocessor

--- HoWestPost.Domain/DeliveryProcessor.cs
+++ HoWestPost.Domain/DeliveryProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 using System.Timers;
 
 namespace HoWestPost.Domain
@@ -9,9 +10,17 @@ namespace HoWestPost.Domain
         #region Globale Variabelen
         private Timer timer;
         public event TiktHandler Tick;
+        public int deliveryTime = 30;
+
+        public DateTime startTime;
+        
+        public int deliveryNumber = 0;
+        public List<Delivery> deliveries = new List<Delivery>();
+        public List<Delivery> sentPackets = new List<Delivery>();
+        public Delivery activeDelivery;
         #endregion
 
-        #region Constructor(s)
+        #region Constructor
 
         public DeliveryProcessor()
         {
@@ -36,7 +45,45 @@ namespace HoWestPost.Domain
         {
             timer.Stop();
         }
-
+        public void AddToWaitinglist(PackageType packageType, int traveltime, bool prior)
+        {
+            double realTravelTime = 0;
+            if (packageType == PackageType.Mini)
+            {
+                Conversie ec = new Conversie(CalcTime.Mini);
+                realTravelTime = ec(traveltime);
+            }
+            else if (packageType == PackageType.Standard)
+            {
+                Conversie ec = new Conversie(CalcTime.Standaard);
+                realTravelTime = ec(traveltime);
+            }
+            else if (packageType == PackageType.Maxi)
+            {
+                Conversie ec = new Conversie(CalcTime.Maxi);
+                realTravelTime = ec(traveltime);
+            }
+            deliveryNumber++;
+            deliveries.Add(new Delivery(packageType, traveltime, prior, deliveryNumber, realTravelTime));
+            deliveries = deliveries.OrderBy(x => x.deliveryNumber).OrderByDescending(p => p.prior).ToList();
+        }
+        public double AverageTime()
+        {
+            double gemiddelde = 0;
+            if (sentPackets.Count() > 0)
+            {                
+                foreach (Delivery d in sentPackets)
+                {
+                    gemiddelde += d.deliveryTime.Subtract(d.startTime).TotalSeconds;
+                }
+                return gemiddelde / sentPackets.Count();
+            }
+            else
+            {
+                return gemiddelde;
+            }
+           
+        }
 
         #endregion
 
--- HoWestPost.UI/MainWindow.xaml.cs
+++ HoWestPost.UI/MainWindow.xaml.cs
@@ -1,22 +1,8 @@
-﻿using DocumentFormat.OpenXml.Spreadsheet;
-using HoWestPost.Domain;
+﻿using HoWestPost.Domain;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-
-using System.Threading.Tasks;
-using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace HoWestPost.UI
@@ -28,14 +14,7 @@ namespace HoWestPost.UI
     {
         #region Global variable
         int counter = 30;
-        int deliveryTime = 30;
-
-        public DateTime startTime;
         DeliveryProcessor deliveryProcessor;
-        int deliveryNumber = 0;
-        List<Delivery> deliveries = new List<Delivery>();
-        List<Delivery> sentPackets = new List<Delivery>();
-        Delivery activeDelivery;
         #endregion
         #region MainWindow
         public MainWindow()
@@ -56,7 +35,6 @@ namespace HoWestPost.UI
             // Toevoegen van event => Link naar methode voor afhandeling
             deliveryProcessor.Tick += DeliveryProcessor_Tick;
             deliveryProcessor.Start();
-
         }
          private void MainWindow_Closed(object sender, System.ComponentModel.CancelEventArgs e)
         {
@@ -73,113 +51,95 @@ namespace HoWestPost.UI
             Dispatcher.Invoke(delegate
             {
                 lblTime.Content = DateTime.Now.ToLongTimeString();
+                lblGemiddelde.Content = deliveryProcessor.AverageTime();
 
-                if ((activeDelivery == null) && (deliveries.Count() > 0))
+
+                if ((deliveryProcessor.activeDelivery == null) && (deliveryProcessor.deliveries.Count() > 0))
                 {
-                    activeDelivery = deliveries.First();
-                    deliveries.Remove(activeDelivery);
-                    startTime = DateTime.Now;
+                    deliveryProcessor.activeDelivery = deliveryProcessor.deliveries.First();
+                    deliveryProcessor.deliveries.Remove(deliveryProcessor.activeDelivery);
+                    deliveryProcessor.startTime = DateTime.Now;
 
-                    UpdateGui();                
+                    UpdateGui();
 
                 }
-                if (activeDelivery != null)
+                if (deliveryProcessor.activeDelivery != null)
                 {
 
-                    double TimeDiverence = DateTime.Now.Subtract(startTime).TotalSeconds;
-                    progressBar.Value = ((TimeDiverence * 10) / activeDelivery.realTravelTime) * 100;
-                    if ((10 * TimeDiverence) <= activeDelivery.realTravelTime)
+                    double TimeDiverence = DateTime.Now.Subtract(deliveryProcessor.startTime).TotalSeconds;
+                    progressBar.Value = ((TimeDiverence * 10) / deliveryProcessor.activeDelivery.realTravelTime) * 100;
+                    if ((10 * TimeDiverence) <= deliveryProcessor.activeDelivery.realTravelTime)
                     {
-                        lblTimeLeft1.Content = activeDelivery.realTravelTime - (10 * TimeDiverence);
+                        lblTimeLeft1.Content = deliveryProcessor.activeDelivery.realTravelTime - (10 * TimeDiverence);
                     }
                     else
                     {
-                        activeDelivery.deliveryTime = DateTime.Now;
-                        sentPackets.Add(activeDelivery);
-                        ListBoxSent.Items.Add(activeDelivery);
-                        activeDelivery = null;
+                        deliveryProcessor.activeDelivery.deliveryTime = DateTime.Now;
+                        deliveryProcessor.sentPackets.Add(deliveryProcessor.activeDelivery);
+                        ListBoxSent.Items.Add(deliveryProcessor.activeDelivery);
+                        deliveryProcessor.activeDelivery = null;
+                        
                         lblTimeLeft1.Content = 0;
                     }
                 }
-                if (sentPackets.Count() > 0)
-                {
-                    double gemiddelde = 0;
-                    foreach (Delivery d in sentPackets)
-                    {
-                        gemiddelde += d.deliveryTime.Subtract(d.startTime).TotalSeconds;
-                    }
-                    lblGemiddelde.Content = gemiddelde / sentPackets.Count();
-                }
+               
+                    
+                
             });
         }
         void UpdateGui()
         {
-            lblPrior.Content = activeDelivery.prior;
-            lblTotalTravelTime.Content = activeDelivery.realTravelTime.ToString() + "min";
-            lblTimeLeft1.Content = activeDelivery.realTravelTime.ToString();
-            lblType.Content = activeDelivery.packageType;
-            lblPacketNumber.Content = activeDelivery.deliveryNumber;
+            lblPrior.Content = deliveryProcessor.activeDelivery.prior;
+            lblTotalTravelTime.Content = deliveryProcessor.activeDelivery.realTravelTime.ToString() + "min";
+            lblTimeLeft1.Content = deliveryProcessor.activeDelivery.realTravelTime.ToString();
+            lblType.Content = deliveryProcessor.activeDelivery.packageType;
+            lblPacketNumber.Content = deliveryProcessor.activeDelivery.deliveryNumber;
             progressBar.Value = 0;
             ListBoxWaiting.Items.Clear();
-            foreach (Delivery d in deliveries)
+            foreach (Delivery d in deliveryProcessor.deliveries)
             {
                 ListBoxWaiting.Items.Add(d);
             }
-
         }
         #endregion
 
-        #region comboDeliveryTime selection 
-        private void ComboxDeliveryTime_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            deliveryTime = Int32.Parse(ComboxDeliveryTime.SelectedItem.ToString());
-        }
-        #endregion
+      
         #region button mini standaard maxi 
         private void ButtonMini_Click(object sender, RoutedEventArgs e)
         {
-            AddToWaitinglist(PackageType.Mini, double.Parse(ComboxDeliveryTime.SelectedItem.ToString()));
+
+            deliveryProcessor.AddToWaitinglist(PackageType.Mini, int.Parse(ComboxDeliveryTime.SelectedItem.ToString()), CheckBoxPrior.IsChecked.Value);
+            UpdateWaitingList();
         }
         private void ButtonStandaard_Click(object sender, RoutedEventArgs e)
         {
-            AddToWaitinglist(PackageType.Standard, double.Parse(ComboxDeliveryTime.SelectedItem.ToString()));
+            deliveryProcessor.AddToWaitinglist(PackageType.Standard, int.Parse(ComboxDeliveryTime.SelectedItem.ToString()), CheckBoxPrior.IsChecked.Value);
+            UpdateWaitingList();
         }
         private void buttonMaxi_Click(object sender, RoutedEventArgs e)
         {
-            AddToWaitinglist(PackageType.Maxi, double.Parse(ComboxDeliveryTime.SelectedItem.ToString()));
+            deliveryProcessor.AddToWaitinglist(PackageType.Maxi, int.Parse(ComboxDeliveryTime.SelectedItem.ToString()), CheckBoxPrior.IsChecked.Value);
+            UpdateWaitingList();
         }
         #endregion
-        private void CheckBoxPrior_Checked(object sender, RoutedEventArgs e)
+        
+        #region UpdateWaitinglist
+        
+        private void UpdateWaitingList ()
         {
-        }
-        #region AddToWaitingList 
-        private void AddToWaitinglist(PackageType packageType, double traveltime)
-        {
-            double realTravelTime = 0;
-            if (packageType == PackageType.Mini ) { 
-                Conversie ec = new Conversie(CalcTime.Mini);
-                realTravelTime = ec(traveltime);
-            }
-            else if (packageType == PackageType.Standard)
-            {
-                Conversie ec = new Conversie(CalcTime.Standaard);
-                realTravelTime = ec(traveltime);
-            }
-            else if (packageType == PackageType.Maxi)
-            {
-                Conversie ec = new Conversie(CalcTime.Maxi);
-                realTravelTime = ec(traveltime);
-            }
-            deliveryNumber++;
-            deliveries.Add(new Delivery(packageType, deliveryTime, CheckBoxPrior.IsChecked.Value, deliveryNumber, realTravelTime));
-            deliveries = deliveries.OrderBy(x => x.deliveryNumber).OrderByDescending(p => p.prior).ToList();  
             ListBoxWaiting.Items.Clear();
-            foreach (Delivery d in deliveries)
+            foreach (Delivery d in deliveryProcessor.deliveries)
             {
                 ListBoxWaiting.Items.Add(d);
             }
         }
         #endregion
 
+        private void ComboxDeliveryTime_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void CheckBoxPrior_Checked(object sender, RoutedEventArgs e)
+        {
+        }
     }
 }
